aee9733d6feb0ed1fb73e6ebc189bbf5ea011ecd
FJCon <conilfernando@gmail.com>
2023-10-30 20:52:54 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Card } from './Card.jsx'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Carga de estudiantes</h1>
+      <form action=""></form>
+      <Card />
+    </div>
+  )
+}
+
+export default App
